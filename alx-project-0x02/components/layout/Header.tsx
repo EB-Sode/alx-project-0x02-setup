ec7e7673cfe099d926b169750070a11b50fc9149
Blessed Sode <emi.square23@gmail.com>
2025-11-18 18:57:52 +0100

    relaod

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import Link from 'next/link';
+import Home from '../../pages/home';
+import About from '../../pages/about';
 
-
-const Header: React.FC = () =>  {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+const Header: React.FC = () => {
+  return (
+    <div>
+      <Home />
+      <About />
+    </div>
+  );
+};
 
 export default Header;
