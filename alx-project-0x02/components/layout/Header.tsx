00f1da70cb138a1ee9e3205e4f52a30980a1c4e1
Blessed Sode <emi.square23@gmail.com>
2025-11-18 18:59:42 +0100

    hive

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import Home from '../../pages/home';
-import About from '../../pages/about';
+import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <div>
-      <Home />
-      <About />
-    </div>
+    <header className="w-full p-4 bg-gray-100 shadow">
+      <nav className="flex gap-6">
+        {/* Link to HOME page */}
+        <Link href="/">
+          Home
+        </Link>
+
+        {/* Link to ABOUT page */}
+        <Link href="/about">
+          About
+        </Link>
+
+        {/* Add more links as needed */}
+      </nav>
+    </header>
   );
 };
 
