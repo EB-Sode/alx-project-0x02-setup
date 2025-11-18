6826281b6768e9da20da986c5b17f72348c9bf38
Blessed Sode <emi.square23@gmail.com>
2025-11-18 20:48:38 +0100

    task 8

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -22,3 +22,17 @@ export interface PostProps {
   title: string;
   content: string;
 }
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+}
+
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: Address;
+}
